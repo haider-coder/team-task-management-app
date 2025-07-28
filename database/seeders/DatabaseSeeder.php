380004d474de7e04e9f68db0778b9aa3edf247b1
haider-coder <haiderjuttearner@gmail.com>
2025-07-28 12:43:45 +0500

    seed updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,26 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
-         \App\Models\User::factory()->create([
-             [
-                       'name'=>'Admin User',
-                       'email'=>'admin@admin.com',
-                       'type'=>1,
-                       'password'=> bcrypt('123456'),
-                    ],
-                    [
-                       'name'=>'Manager User',
-                       'email'=>'manager@manager.com',
-                       'type'=> 2,
-                       'password'=> bcrypt('123456'),
-                    ],
-                    [
-                       'name'=>'User',
-                       'email'=>'user@user.com',
-                       'type'=>0,
-                       'password'=> bcrypt('123456'),
-                    ],
-         ]);
+      \App\Models\User::insert([
+         'name'=>'Admin User',
+         'email'=>'admin@admin.com',
+         'type'=>1,
+         'password'=> bcrypt('123456'),
+      ]);
     }
 }
