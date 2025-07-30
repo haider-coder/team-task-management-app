f8e5158dfe6486c0946145aaf993f03fee485b41
haider-coder <haiderjuttearner@gmail.com>
2025-07-30 11:41:57 +0500

    create task page header changed

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,11 +5,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('Create Task') }}</div>
 
                     <div class="card-body">
-                        <h2>Create Task</h2>
-
+             
                         <form method="post" action="{{ route('admin.tasks.store') }}">
                             @csrf
                             <div class="form-group">
